c9c9380f657a9619ef7e7405aeb5c77389bb897b
H8872 <H8872@student.jamk.fi>
2016-01-13 13:38:11 +0200

    Initial commit.

---
+++ D1T7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D1T7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vuosi, jako;
+            Console.Write("Anna vuosiluku > ");
+            vuosi = int.Parse(Console.ReadLine());
+            jako = vuosi % 4;
+            if (jako == 0)
+            {
+
+            }
+        }
+    }
+}
